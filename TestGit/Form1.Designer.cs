868efff21b484125e6e8a6a76cf62cebaa246db2
Edgar Felipe Hernandez Garcia <loquendero2010@live.com.mx>
2025-04-24 13:25:20 -0600

    bt1

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 102);
+            this.button1.Location = new System.Drawing.Point(104, 98);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(108, 64);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
