b48e5f308798967e493bdd9ae6ceba99d75598ed
Edgar Felipe Hernandez Garcia <loquendero2010@live.com.mx>
2025-04-24 13:12:00 -0600

    Form2

--- TestGit/Form2.Designer.cs
+++ TestGit/Form2.Designer.cs
@@ -28,10 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(718, 513);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
 
         }
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
